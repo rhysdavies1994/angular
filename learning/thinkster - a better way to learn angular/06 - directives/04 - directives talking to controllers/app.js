e43b6de49747488b33a9f4209c46cb9b1eea8f27
Rhys Davies <rhysdavies1994@gmail.com>
2016-05-20 16:25:12 +0800

    Completed thinkster tutorials up to 6.6 - transclusion

--- learning/thinkster - a better way to learn angular/06 - directives/04 - directives talking to controllers/app.js
+++ learning/thinkster - a better way to learn angular/06 - directives/04 - directives talking to controllers/app.js
@@ -1,13 +1,21 @@
-angular.module('functionalities', [])
+function FunCtrl(){
+    var self = this;
+
+    self.start = function(){
+        console.log("Fun times have been started!");
+    }
+
+    self.end = function(){
+        console.log("Fun time is over.");
+    }
+}
+
+angular.module('coolApp', [])
+    .controller("FunCtrl", FunCtrl)
     .directive("entering", function () {
         return function (scope, element, attrs) {
             element.bind("mouseenter", function(){
-            })
-        }
-    })
-    .directive("leaving", function(){
-        return function(scope, element, attrs){
-            element.bind("mouseleave", function(){
+                scope.$apply(attrs.entering);
             })
         }
     })
---
+++ learning/thinkster - a better way to learn angular/06 - directives/06 - transclusion/app.js
@@ -0,0 +1,9 @@
+angular.module('greetings', [])
+    .directive("welcome", function() {
+        return {
+            restrict: "E",
+            scope: {},
+            transclude: true,
+            template: "<div>This is the welcome component</div><ng-transclude></ng-transclude>"
+        }
+    });
